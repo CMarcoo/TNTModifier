38eeaa127d25e4520c7af0de0da55a1f6f084a54
TheViperShow <thevipershowita@gmail.com>
2020-07-26 12:36:24 +0200

    Fixed minor things.

--- src/main/java/me/thevipershow/tntmodifier/listeners/InteractionListener.kt
+++ src/main/java/me/thevipershow/tntmodifier/listeners/InteractionListener.kt
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.entity.ExplosionPrimeEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 import java.util.concurrent.ThreadLocalRandom
 
 class InteractionListener : Listener {
@@ -20,6 +21,8 @@ class InteractionListener : Listener {
         val action = event.action
         if (action != Action.RIGHT_CLICK_BLOCK) return
 
+        if (event.isBlockInHand) return
+
         val clickedBlock = event.clickedBlock
         if (clickedBlock.type != Material.TNT) return
 
@@ -28,7 +31,7 @@ class InteractionListener : Listener {
         }
 
         clickedBlock.type = Material.AIR
-        val adjustedSpawnLocation = clickedBlock.location.add(0.50, 0.50, 0.50)
+        val adjustedSpawnLocation = clickedBlock.location.add(0.50, 0.05, 0.50)
         clickedBlock.world.spawnEntity(adjustedSpawnLocation, EntityType.PRIMED_TNT)
     }
 
@@ -45,8 +48,9 @@ class InteractionListener : Listener {
                 val world = location.world
 
                 world.playSound(location, Values.fuseFailSoundType, 10.0f, 1.0f)
+                world.dropItem(location.add(0.0, 0.15, 0.0), ItemStack(Material.TNT, 1))
                 for (i in 1..50)
-                    world.spawnParticle(Values.fuseFailEffectType, location.x, location.y, location.z, 1, 1.5, 1.5, 1.5, 0.5, null)
+                    world.spawnParticle(Values.fuseFailEffectType, location.x, location.y, location.z, 1, 1.0, 1.0, 1.0, 0.35, null)
             }
         }
     }
